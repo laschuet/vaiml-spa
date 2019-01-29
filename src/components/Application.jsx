6a519d8aa7061e7fcdc54f1a061431501b68b19b
laschuet <laschuet@users.noreply.github.com>
2019-01-29 19:55:04 +0100

    Add more contribution metadata

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -3,13 +3,37 @@ import React from 'react';
 import Contribution from './Contribution';
 import WebSocketTest from './WebSocketTest';
 
-const Application = () => (
-  <div>
-    <WebSocketTest />
-    <hr />
-    <Contribution author="Author 1">Content</Contribution>
-    <Contribution author="Author 2">Other content</Contribution>
-  </div>
-);
+const Application = () => {
+  const contributions = [
+    {
+      id: 1,
+      author: 'Me',
+      text: 'Just a string',
+      timestamp: '2019-01-29 10:05:15',
+    },
+    {
+      id: 2,
+      author: 'You',
+      text: 'Two words',
+      timestamp: '2019-01-30 23:30:05',
+    },
+  ];
+
+  return (
+    <div>
+      <WebSocketTest />
+      <br />
+      {contributions.map(contribution => (
+        <Contribution
+          key={contribution.id}
+          id={contribution.id}
+          author={contribution.author}
+          timestamp={contribution.timestamp}>
+          {contribution.text}
+        </Contribution>
+      ))}
+    </div>
+  );
+};
 
 export default Application;
--- src/components/Contribution.jsx
+++ src/components/Contribution.jsx
@@ -3,7 +3,9 @@ import PropTypes from 'prop-types';
 
 const Contribution = props => (
   <div>
-    <div>{props.author}</div>
+    <div>
+      {props.author} &middot; {props.timestamp} &middot; #{props.id}
+    </div>
     <div>{props.children}</div>
   </div>
 );
@@ -11,6 +13,8 @@ const Contribution = props => (
 Contribution.propTypes = {
   author: PropTypes.string.isRequired,
   children: PropTypes.node.isRequired,
+  id: PropTypes.number.isRequired,
+  timestamp: PropTypes.string.isRequired,
 };
 
 export default Contribution;
