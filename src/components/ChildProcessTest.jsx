0828ad3bd131beaa0e9ee67dff23685815f92ac6
laschuet <laschuet@users.noreply.github.com>
2019-11-14 23:11:43 +0100

    Finish child process test component

--- src/components/ChildProcessTest.jsx
+++ src/components/ChildProcessTest.jsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { useWebSocket } from '../hooks/useWebSocket';
 
 const ChildProcessTest = () => {
+  const [input, setInput] = useState('');
+  const [message, sendMessage] = useWebSocket('ws://127.0.0.1:4020');
+
+  const handleChange = evt => {
+    setInput(evt.target.value);
+  };
+
   const handleSubmit = evt => {
     evt.preventDefault();
+    sendMessage(JSON.stringify({ type: 'run_julia_code', data: input }));
+    setInput('');
   };
 
   return (
@@ -11,13 +22,36 @@ const ChildProcessTest = () => {
         <div className="Box-title">ChildProcessTest</div>
       </div>
       <div className="Box-body">
-        <div>Output:</div>
-        <div />
-      </div>
-      <div className="Box-footer clearfix">
-        <button className="btn btn-primary float-right" onClick={handleSubmit}>
-          Run
-        </button>
+        <form>
+          <div className="input-group">
+            <input
+              className="form-control"
+              id="input"
+              type="text"
+              value={input}
+              onChange={handleChange}
+              placeholder="Julia program code"
+            />
+            <span className="input-group-button">
+              <button
+                className="btn btn-primary"
+                type="submit"
+                onClick={handleSubmit}>
+                Send
+              </button>
+            </span>
+          </div>
+          <div className="form-group">
+            <textarea
+              className="form-control"
+              id="example-textarea"
+              placeholder="Julia program output"
+              disabled
+              value={message && JSON.parse(message).data}
+            />
+          </div>
+          <div>{message}</div>
+        </form>
       </div>
     </div>
   );
